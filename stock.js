ca96e43824262fb442c665e56b00d567a6465b31
MarrocchiProgrammer <tomasmarrocchi@gmail.com>
2023-03-28 19:33:58 -0300

    comiteando ando

---
+++ stock.js
@@ -0,0 +1,86 @@
+let stockProductos = [
+    {
+        id: 1,
+        nombre: "Criolla LaAlpujarra",
+        cantidad : 1,
+        precio: 70000,
+        img: "./guitar-images/guitarra-criolla/Guitarra-Criolla-LaAlpujarra.png"
+    },
+    {
+        id: 2,
+        nombre: "Criolla Midiplus",
+        cantidad : 1,
+        precio: 180000,
+        img: "./guitar-images/guitarra-criolla/Guitarra-Criolla-Midiplus.png"
+    },
+    {
+        id: 3,
+        nombre: "Criolla Takamine",
+        cantidad : 1,
+        precio:  40000,
+        img: "./guitar-images/guitarra-criolla/Guitarra-Criolla-Takamine.png"
+    },
+    {
+        id: 4,
+        nombre: "Acustica Cort",
+        cantidad : 1,
+        precio: 125000,
+        img: "./guitar-images/guitarra-acustica/Guitarra-Acustica-Cort.png"
+    },
+    {
+        id: 5,
+        nombre: "Acustica Ivanez",
+        cantidad : 1,
+        precio: 170000,
+        img: "./guitar-images/guitarra-acustica/Guitarra-Acustica-Ivanez.png"
+    },
+    {
+        id: 6,
+        nombre: "Acustica Parquer",
+        cantidad : 1,
+        precio: 85000,
+        img: "./guitar-images/guitarra-acustica/Guitarra-Acustica-Parquer.png"
+    },
+    {
+        id: 7,
+        nombre: "Electroacustica Cort",
+        cantidad : 1,
+        precio: 90000,
+        img: "./guitar-images/guitarra-electroacustica/Guitarra-ElectroAcustica-Cort.png"
+    },
+    {
+        id: 8,
+        nombre: "Electroacustica Mahogany",
+        cantidad : 1,
+        precio: 75000,
+        img: "./guitar-images/guitarra-electroacustica/Guitarra-Electroacustica-Mahogany.png"
+    },
+    {
+        id: 9,
+        nombre: "Electroacustica Sigma",
+        cantidad : 1,
+        precio: 145000,
+        img: "./guitar-images/guitarra-electroacustica/Guitarra-Electroacustica-Sigma.png"
+    },
+    {
+        id: 10,
+        nombre: "Electrica Epiphone",
+        cantidad : 1,
+        precio: 250000,
+        img: "./guitar-images/guitarra-electrica/Guitarra-Electrica-Epiphone.png"
+    },
+    {
+        id: 11,
+        nombre: "Electrica Ibanez",
+        cantidad : 1,
+        precio: 375000,
+        img: "./guitar-images/guitarra-electrica/Guitarra-Electrica-Ibanez.png"
+    },
+    {
+        id: 12,
+        nombre: "Electrica Tagima",
+        cantidad : 1,
+        precio: 100000,
+        img: "./guitar-images/guitarra-electrica/Guitarra-Electrica-Tagima.png"
+    }
+]
